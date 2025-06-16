95a4cf67ba382b8c7c0c820bd5f1a85ea1cef55d
ENIADE JESSICA AYODAMOLA <Jessica.eniade@bingham.edu.ng>
2025-06-16 23:38:12 +0100

    csc assignment

---
+++ account.py
@@ -0,0 +1,12 @@
+class Account:
+    def _init_(self, balance):
+        self.balance = balance
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            raise ValueError("Insufficient funds")
+
+    def deposit(self, amount):
+        self.balance += amount
